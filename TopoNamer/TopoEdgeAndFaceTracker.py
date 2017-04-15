fe07c5ca1e97d73916db36b1016c8c10fca2dbcf
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2017-04-14 21:19:36 -0400

    Moved a bunch of stuff around.

--- TopoNamer/TopoEdgeAndFaceTracker.py
+++ TopoNamer/TopoEdgeAndFaceTracker.py
@@ -150,31 +150,3 @@ class TopoEdgeAndFaceTracker(object):
 
         self._faceNames[oldFaceName] = oldFace
 
-class TopoNamer(object):
-
-    """This class manages the topological naming of an OCC Shape object"""
-
-    def __init__(self):
-        self._tracker = TopoEdgeAndFaceTracker()
-
-    def addShape(self, feature):
-        """Track a created Shape's topology
-
-        :feature: A FreeCAD PartFeature
-        """
-        shape = feature.Shape
-        for face in shape.Faces:
-            self._tracker.addFace(face)
-
-    def modifyShape(self, newFaces=None, modifiedFaces=None, deletedFaces=None):
-        if all([i is None for i in [newFaces, modifiedFaces, deletedFaces]]):
-            msg = 'At least one of newFaces, modifiedFaces, or deletedFaces must be provided'
-            raise ValueError(msg)
-
-        if not newFaces is None:
-            for face in newFaces:
-                self._tracker.addFace(face)
-
-        if not modifiedFaces is None:
-            for oldFaceName, newFace in modifiedFaces:
-                self._tracker.modifyFace(oldFaceName, newFace)
--- test/TopoNamingTester.py
+++ test/TopoNamingTester.py
@@ -2,46 +2,7 @@ import unittest
 from unittest import mock
 from TopoNamer import TopoNamer, TopoEdgeAndFaceTracker
 from TestingHelpers import MockObjectMaker
-from TrackedFace import TrackedFace
 from TrackedEdge import TrackedEdge
-
-class TestTrackedFace(unittest.TestCase):
-    def setUp(self):
-        self.maker = MockObjectMaker()
-        self.mock_face0 = self.maker.OCCFace()
-        self.trackedFace = TrackedFace(self.mock_face0, 'Face000')
-
-    def test_getOCCFace(self):
-        fetchedFace = self.trackedFace.getOCCFace()
-        self.assertTrue(fetchedFace.isEqual(self.mock_face0))
-
-    def test_getName(self):
-        fetchedName = self.trackedFace.getName()
-        self.assertEqual(fetchedName, 'Face000')
-
-    def test_createNewTrackedFace(self):
-        self.assertEqual(self.mock_face0, self.trackedFace._occFace)
-        self.assertEqual('Face000', self.trackedFace._name)
-        self.assertEqual([0,1,2,3], self.trackedFace._unsharedEdges)
-
-    def test_isSharedEdgeTrue(self):
-        isShared = self.trackedFace.isEdgeShared(self.mock_face0.Edges[0])
-        self.assertTrue(isShared)
-
-    def test_isSharedEdgeFalse(self):
-        mock_edge0 = self.maker.OCCEdge()
-
-        isShared = self.trackedFace.isEdgeShared(mock_edge0)
-        self.assertFalse(isShared)
-
-    def test_updateUnsharedEdge(self):
-        self.trackedFace.updateUnsharedEdge(self.mock_face0.Edges[0])
-        self.assertEqual(self.trackedFace._unsharedEdges, [1,2,3])
-
-    def test_updateUnsharedEdgeError(self):
-        self.trackedFace._unsharedEdges = [1,2,3]
-
-        self.assertRaises(ValueError, self.trackedFace.updateUnsharedEdge, self.mock_face0.Edges[0])
 
 class TestTrackedEdge(unittest.TestCase):
 
