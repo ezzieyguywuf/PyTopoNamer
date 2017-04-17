542e227bac6db43521f46d04898b3f05902fccbe
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2017-04-17 14:35:29 -0400

    Added failing test for `getEdgeName`

--- test/testTopoTracker.py
+++ test/testTopoTracker.py
@@ -30,6 +30,18 @@ class TestTracker(unittest.TestCase):
         self.assertEqual(name2, 'Face001aaa')
         self.assertEqual(name3, 'Edge002aa')
 
+    def test_getEdgeName(self):
+        mock_face0 = self.maker.OCCFace()
+        mock_face1 = self.maker.OCCFace()
+        mock_face1.Edges[0] = mock_face0.Edges[0]
+
+        self.tracker.addFace(mock_face0)
+        self.tracker.addFace(mock_face1)
+
+        edgeName = self.tracker.getEdgeName(mock_face0.Edges[0])
+        self.assertEqual(edgeName, 'Edge000')
+
+
     def test_addTwoFaces_noSharedEdges(self):
         mock_face0 = self.maker.OCCFace()
         mock_face1 = self.maker.OCCFace()
@@ -90,20 +102,31 @@ class TestTracker(unittest.TestCase):
         self.assertEqual(self.tracker._edgeTrackers[0]._occEdge, mock_face0.Edges[0])
         self.assertEqual(self.tracker._edgeTrackers[0]._faceNames, ['Face000', 'Face001'])
 
-    def test_modifyFaceWithNoSameEdges(self):
-        mock_face0 = self.maker.OCCFace()
-        mock_face1a = self.maker.OCCFace()
-        mock_face1b = self.maker.OCCFace()
-        mock_face1a.Edges[0] = mock_face0.Edges[0]
+    # def test_modifyFaceWithSplitFace(self):
+        # '''An example of this is a square face that turns into a U-shaped face.'''
+        # mock_face0a = self.maker.OCCFace()
+        # mock_face0b= self.maker.OCCFace()
+        # mock_face1a = self.maker.OCCFace()
+        # mock_face1b = self.maker.OCCFace()
 
-        self.tracker.addFace(mock_face0)
-        self.tracker.addFace(mock_face1a)
-        self.tracker.modifyFace(mock_face1a, mock_face1b)
+        # mock_face1a.Edges[0] = mock_face0a.Edges[0]
 
-        trackedEdgesValues = []
-        for edgeTracker in self.tracker._edgeTrackers:
-            trackedEdgesValues.append(edgeTracker.getOCCEdge().value)
+        # # the edge between mock_face0 and mock_face1 is getting split, thus creating a
+        # # fifth edge on each face.
+        # mock_face0b.Edges.append(self.maker.OCCEdge())
+        # mock_face1b.Edges.append(self.maker.OCCEdge())
+        # mock_face1b.Edges[-2] = mock_face0b.Edges[-2]
+        # mock_face1b.Edges[-1] = mock_face0b.Edges[-1]
 
-        checkEdges = mock_face0.Edges + mock_face1b.Edges
-        checkEdgesValues = [i.value for i in checkEdges]
-        self.assertEqual(checkEdgesValues, trackedEdgesValues)
+        # self.tracker.addFace(mock_face0a)
+        # self.tracker.addFace(mock_face1a)
+        # self.tracker.modifyFace(mock_face0a, mock_face0b)
+        # self.tracker.modifyFace(mock_face1a, mock_face1b)
+
+        # trackedEdgesValues = []
+        # for edgeTracker in self.tracker._edgeTrackers:
+            # trackedEdgesValues.append(edgeTracker.getOCCEdge().value)
+
+        # checkEdges = mock_face0.Edges + mock_face1b.Edges
+        # checkEdgesValues = [i.value for i in checkEdges]
+        # self.assertEqual(checkEdgesValues, trackedEdgesValues)
