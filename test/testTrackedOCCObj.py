16778f6fcbb6c0ed7f9be2c86e68854cb452cdcc
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2017-04-18 10:46:30 -0400

    Added more tests for base class.

--- test/testTrackedOCCObj.py
+++ test/testTrackedOCCObj.py
@@ -16,3 +16,19 @@ class TestTrackedOCCObj(unittest.TestCase):
         fetchedName = self.trackedOCCObj.getName()
         self.assertEqual(fetchedName, 'Object000')
 
+    def test_getParent(self):
+        mock_obj = self.maker.OCCFace()
+        trackedObj = TrackedOCCObj(mock_obj, 'Object001', self.trackedOCCObj)
+        self.assertEqual(trackedObj.getParent(), self.trackedOCCObj)
+
+    def test_isChildOf(self):
+        mock_obj = self.maker.OCCFace()
+        trackedObj = TrackedOCCObj(mock_obj, 'Object001', self.trackedOCCObj)
+        self.assertTrue(trackedObj.isChildOf(self.trackedOCCObj))
+
+    def test_isNotChildOf(self):
+        mock_obj = self.maker.OCCFace()
+        mock_obj2 = self.maker.OCCFace()
+        trackedObj = TrackedOCCObj(mock_obj, 'Object001')
+        trackedObj2 = TrackedOCCObj(mock_obj2, 'Object002', parent=mock_obj)
+        self.assertFalse(trackedObj2.isChildOf(self.trackedOCCObj))
